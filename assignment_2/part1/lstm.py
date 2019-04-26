4ef4220f27ae851b612de71034d378ffb491bebf
Vansil <silvandeboer@gmail.com>
2019-04-26 13:59:29 +0200

    print header

--- assignment_2/part1/lstm.py
+++ assignment_2/part1/lstm.py
@@ -18,6 +18,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import numpy as np
 import torch
 import torch.nn as nn
 
@@ -25,10 +26,61 @@ import torch.nn as nn
 
 class LSTM(nn.Module):
 
-    def __init__(self, seq_length, input_dim, num_hidden, num_classes, batch_size, device='cpu'):
+    def __init__(self, input_dim, num_hidden, num_classes, batch_size, device='cpu'):
         super(LSTM, self).__init__()
-        # Initialization here ...
 
-    def forward(self, x):
-        # Implementation here ...
-        pass
+        # Weight initialisation
+        init_range = lambda fan_in, fan_out : np.sqrt(6 / (fan_in + fan_out))
+        init_Wgx = init_Wix = init_Wfx = init_Wox = init_range(num_hidden, input_dim)
+        init_Wgh = init_Wih = init_Wfh = init_Woh = init_range(num_hidden, num_hidden)
+
+        self.Wgx = nn.Parameter(torch.Tensor(input_dim, num_hidden).uniform_(-init_Wgx, init_Wgx))
+        self.Wgh = nn.Parameter(torch.Tensor(num_hidden, num_hidden).uniform_(-init_Wgh, init_Wgh))
+
+        self.Wix = nn.Parameter(torch.Tensor(input_dim, num_hidden).uniform_(-init_Wix, init_Wix))
+        self.Wih = nn.Parameter(torch.Tensor(num_hidden, num_hidden).uniform_(-init_Wih, init_Wih))
+
+        self.Wfx = nn.Parameter(torch.Tensor(input_dim, num_hidden).uniform_(-init_Wfx, init_Wfx))
+        self.Wfh = nn.Parameter(torch.Tensor(num_hidden, num_hidden).uniform_(-init_Wfh, init_Wfh))
+
+        self.Wox = nn.Parameter(torch.Tensor(input_dim, num_hidden).uniform_(-init_Wox, init_Wox))
+        self.Woh = nn.Parameter(torch.Tensor(num_hidden, num_hidden).uniform_(-init_Woh, init_Woh))
+
+        self.Wph = nn.Parameter(torch.Tensor(num_hidden, num_classes).zero_())
+
+        # initialise biases
+        self.bg = nn.Parameter(torch.Tensor(num_hidden).zero_())
+        self.bi = nn.Parameter(torch.Tensor(num_hidden).zero_())
+        self.bf = nn.Parameter(torch.Tensor(num_hidden).zero_())
+        self.bo = nn.Parameter(torch.Tensor(num_hidden).zero_())
+
+        self.bp = nn.Parameter(torch.Tensor(num_classes).zero_())
+
+        # Weights and biases to device
+        self.to(device)
+
+        # initialise cell state and hidden layer
+        self.state = torch.Tensor(batch_size, num_hidden).zero_().to(device)
+        self.hidden = torch.Tensor(batch_size, num_hidden).zero_().to(device)
+
+
+    def forward(s, x):
+        # shape input x:  (sequence length, batch size, input size=1)
+        # shape output p: (batch size, class)
+        
+
+
+        for t in range(x.shape[0]):
+            # Gates
+            g = torch.tanh(x[t] @ s.Wgx + s.hidden @ s.Wgh + s.bg)
+            i = torch.sigmoid(x[t] @ s.Wix + s.hidden @ s.Wih + s.bi)
+            f = torch.sigmoid(x[t] @ s.Wfx + s.hidden @ s.Wfh + s.bf)
+            o = torch.sigmoid(x[t] @ s.Wox + s.hidden @ s.Woh + s.bo)
+            # update state and hidden layer
+            s.state = g * i + s.state * f
+            s.hidden = torch.tanh(s.state) * o
+
+        # Compute output only in last layer
+        out = s.hidden @ s.Wph + s.bp
+
+        return out
--- assignment_2/part1/train.py
+++ assignment_2/part1/train.py
@@ -51,7 +51,8 @@ def train(config):
         model = VanillaRNN(config.input_dim, config.num_hidden, \
             config.num_classes, config.batch_size, device)
     else:
-        raise NotImplementedError
+        model = LSTM(config.input_dim, config.num_hidden, \
+            config.num_classes, config.batch_size, device)
 
     # Initialize the dataset and data loader (note the +1)
     dataset = PalindromeDataset(config.input_length+1)
@@ -131,7 +132,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     # Model params
-    parser.add_argument('--model_type', type=str, default="RNN", help="Model type, should be 'RNN' or 'LSTM'")
+    parser.add_argument('--model_type', type=str, default="LSTM", help="Model type, should be 'RNN' or 'LSTM'")
     parser.add_argument('--input_length', type=int, default=10, help='Length of an input sequence')
     parser.add_argument('--input_dim', type=int, default=1, help='Dimensionality of input sequence')
     parser.add_argument('--num_classes', type=int, default=10, help='Dimensionality of output sequence')
@@ -151,7 +152,10 @@ if __name__ == "__main__":
     else:
         # Conduct experiment
         # Vary input length and evaluate accuracy and loss over time
-        file_out = 'output/experiment_results.py'
+        file_out = 'output/experiment_results.txt'
+
+        with open(file_out, "a") as f:
+            f.write("len;losses;accuracies")
 
         # run until external time limit
         input_length = 3
